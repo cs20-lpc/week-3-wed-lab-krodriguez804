8d2bd1b7fba84e2f55f2827b4a0d72d0d5f57d53
krodriguez804 <krodriguez804@gmail.com>
2026-02-07 22:52:25 -0800

    Attempt 1

--- lab04/driver.cpp
+++ lab04/driver.cpp
@@ -1,1 +1,50 @@
 // TO DO: Implement the driver main() function to test your code.
+// Krystle Dao Week 3 Wed Lab
+
+#include <iostream>
+#include <string>
+#include "LinkedList.hpp"
+#include "List.hpp"
+
+using namespace std;
+
+int main() {
+
+    LinkedList<int> intList;
+
+    cout << "Is list empty? " << intList.isEmpty() << endl;
+
+    cout << "Length: " << intList.getLength() << endl;
+
+    intList.append(12);
+
+    cout << intList << endl;
+
+    intList.append(95);
+
+    intList.append(108);
+
+    cout << intList << endl;
+
+    cout << intList.getElement(0) << endl;
+
+    cout << intList.getElement(1) << endl;
+
+    cout << intList.getElement(2) << endl;
+
+    cout << "Is list empty? " << intList.isEmpty() << endl;
+
+    cout << "Length: " << intList.getLength() << endl;
+
+    intList.replace(1, 214);
+
+    cout << intList << endl;
+
+    intList.clear();
+
+    cout << "List is empty: " << intList.isEmpty() << endl;
+
+    cout << "Length: " << intList.getLength() << endl;
+
+    return 0;
+}
